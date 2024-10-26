ae5a2e190c9e4c6566caa908033f833fa4a9cdee
Artavazd Balaian <reasyu@gmail.com>
2024-10-26 14:49:50 +0800

    feat: Add slow `write_to_disk`

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ use chrono::{DateTime, Utc};
 use serde::Serialize;
 use std::net::SocketAddr;
 use std::time::Duration;
+use tokio::io::AsyncWriteExt;
 use tokio::signal;
-use tokio_util::sync::CancellationToken;
 use tower::{BoxError, ServiceBuilder};
-use tracing::info;
+use tracing::{info, warn};
 
 shadow!(build);
 
@@ -41,18 +41,32 @@ struct ErrorMessage {
     message: String,
 }
 
+async fn write_to_disk(path: &str) -> errors::Result<()> {
+    let mut file = tokio::fs::File::create(path).await?;
+    for i in 1..100 {
+        file.write_all(format!("{}\n", i).as_bytes()).await?;
+        tokio::time::sleep(Duration::from_millis(20)).await;
+    }
+    file.flush().await?;
+    Ok(())
+}
+
 async fn info_handler() -> errors::Result<Json<Info>> {
     info!("Starting executing `info_handler`");
     let info = Info {
         version: APP_VERSION.to_string(),
         current_timestamp: Utc::now(),
     };
+    write_to_disk("data.txt").await?;
     info!("Finished executing `info_handler`");
     Ok(Json(info))
 }
 
+const TIMEOUT_DURATION: Duration = Duration::from_secs(1);
+
 async fn handle_timeout_error(err: BoxError) -> (StatusCode, Json<ErrorMessage>) {
     if err.is::<tower::timeout::error::Elapsed>() {
+        warn!("Slow request timed out");
         (
             StatusCode::REQUEST_TIMEOUT,
             Json(ErrorMessage {
@@ -60,7 +74,7 @@ async fn handle_timeout_error(err: BoxError) -> (StatusCode, Json<ErrorMessage>)
                 reason: StatusCode::REQUEST_TIMEOUT
                     .canonical_reason()
                     .map(|s| s.to_string()),
-                message: "Request took too long".to_string(),
+                message: format!("Request took longer than {:?}", TIMEOUT_DURATION),
             }),
         )
     } else {
@@ -79,12 +93,11 @@ async fn handle_timeout_error(err: BoxError) -> (StatusCode, Json<ErrorMessage>)
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> errors::Result<()> {
-    let token = CancellationToken::new();
     logger::setup("INFO");
     let route = Router::new().route("/version", get(info_handler)).layer(
         ServiceBuilder::new()
             .layer(HandleErrorLayer::new(handle_timeout_error))
-            .timeout(Duration::from_secs(2))
+            .timeout(TIMEOUT_DURATION)
             .into_inner(),
     );
 
@@ -94,7 +107,7 @@ async fn main() -> errors::Result<()> {
     let svc = route.into_make_service_with_connect_info::<SocketAddr>();
     let http_listener = tokio::net::TcpListener::bind(http_addr).await.unwrap();
     axum::serve(http_listener, svc.clone())
-        .with_graceful_shutdown(shutdown_signal(token.clone()))
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .expect("Failed to start server");
     info!("Server shutdown");
@@ -102,7 +115,7 @@ async fn main() -> errors::Result<()> {
     Ok(())
 }
 
-async fn shutdown_signal(token: CancellationToken) {
+async fn shutdown_signal() {
     let ctrl_c = async {
         signal::ctrl_c()
             .await
@@ -122,10 +135,8 @@ async fn shutdown_signal(token: CancellationToken) {
 
     tokio::select! {
         _ = ctrl_c => {
-            token.cancel()
         },
         _ = terminate => {
-            token.cancel()
         },
     }
 
